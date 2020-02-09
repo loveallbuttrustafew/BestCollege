d5feab18ba4988e9627a42c76f418551252d7b67
loveallbuttrustafew <loveallbuttrustafew@gmail.com>
2020-02-09 10:40:47 +0300

    add user repository

---
+++ src/main/java/uoggmk/college/Repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package uoggmk.college.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uoggmk.college.Models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
