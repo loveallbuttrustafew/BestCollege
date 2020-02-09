2141e254955fc6f7b7361b8cf65430b6a3494161
loveallbuttrustafew <loveallbuttrustafew@gmail.com>
2020-02-09 15:40:31 +0300

    add student and teacher controller

---
+++ src/main/java/uoggmk/college/Controllers/StudentController.java
@@ -0,0 +1,12 @@
+package uoggmk.college.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudentController {
+    @GetMapping("/student")
+    public String main() {
+        return "student/main";
+    }
+}
