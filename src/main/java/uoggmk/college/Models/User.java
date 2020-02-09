8cefdaeda26dd8dd1974d43aac54a38ec7cced28
loveallbuttrustafew <loveallbuttrustafew@gmail.com>
2020-02-09 10:39:53 +0300

    add user model

---
+++ src/main/java/uoggmk/college/Models/User.java
@@ -0,0 +1,20 @@
+package uoggmk.college.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String studentsBookNumber;
+    private String phoneNumber;
+}
