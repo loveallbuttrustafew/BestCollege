ac2fc25178228b74876fcfbb620dae82ba6b9a2f
loveallbuttrustafew <loveallbuttrustafew@gmail.com>
2020-02-09 15:37:10 +0300

    reformat

--- src/main/java/uoggmk/college/CollegeApplication.java
+++ src/main/java/uoggmk/college/CollegeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CollegeApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(CollegeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeApplication.class, args);
+    }
 }
