d36137aed9b59c83858ddd6b6254b6608b3598bd
loveallbuttrustafew <loveallbuttrustafew@gmail.com>
2020-02-09 15:38:54 +0300

    reformat

--- src/main/java/uoggmk/college/Controllers/IndexController.java
+++ src/main/java/uoggmk/college/Controllers/IndexController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 }
