94b06412a2e8d4fd7535cd722c88169acea7077d
loveallbuttrustafew <loveallbuttrustafew@gmail.com>
2020-02-11 12:58:26 +0300

    allow access to admin addUser

--- src/main/java/uoggmk/college/WebSecurityConfiguration.java
+++ src/main/java/uoggmk/college/WebSecurityConfiguration.java
@@ -22,6 +22,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/admin").hasAuthority("ADMIN")
+                .antMatchers("/admin/user/add").hasAuthority("ADMIN")
                 .antMatchers("/student").hasAuthority("STUDENT")
                 .antMatchers("/teacher").hasAuthority("TEACHER")
                 .anyRequest().authenticated()
