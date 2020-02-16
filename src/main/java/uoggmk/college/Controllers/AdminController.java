acede0770dc490187ee9d33c2ef10b21dc4ec4b5
loveallbuttrustafew <loveallbuttrustafew@gmail.com>
2020-02-16 14:01:12 +0300

    admin controller edits

--- src/main/java/uoggmk/college/Controllers/AdminController.java
+++ src/main/java/uoggmk/college/Controllers/AdminController.java
@@ -5,15 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import uoggmk.college.Models.Group;
 import uoggmk.college.Models.Role;
 import uoggmk.college.Models.User;
+import uoggmk.college.Services.Exceptions.GroupAlreadyExistsException;
+import uoggmk.college.Services.Exceptions.GroupNotFoundException;
 import uoggmk.college.Services.Exceptions.UserAlreadyExistsException;
+import uoggmk.college.Services.GroupService;
 import uoggmk.college.Services.UserService;
 
 @Controller
 public class AdminController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private GroupService groupService;
 
     @GetMapping("/admin")
     public String main() {
@@ -21,10 +27,15 @@ public class AdminController {
     }
 
     @PostMapping("/admin/user/add")
-    public String addUser(@RequestParam("typeuser") Role type, @RequestParam("firstname") String firstName,
-                          @RequestParam("secondname") String secondName, @RequestParam("lastname") String lastName,
-                          @RequestParam("username") String username, @RequestParam("password") String password,
-                          @RequestParam("phonenumber") String phoneNumber, @RequestParam("studentbook") String studentBook){
+    public String addUser(@RequestParam("typeuser") Role type,
+                          @RequestParam("firstname") String firstName,
+                          @RequestParam("secondname") String secondName,
+                          @RequestParam("lastname") String lastName,
+                          @RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          @RequestParam(value = "phonenumber", required = false) String phoneNumber,
+                          @RequestParam(value = "studentbook", required = false) String studentBook,
+                          @RequestParam(value = "groupid", required = false) Long groupId) {
         User user = new User();
         user.setRole(type);
         user.setEnabled(true);
@@ -33,14 +44,46 @@ public class AdminController {
         user.setLastName(lastName);
         user.setUsername(username);
         user.setPassword(password);
-        user.setPhoneNumber(phoneNumber);
-        user.setStudentsBookNumber(studentBook);
+
+        if (phoneNumber != null)
+            user.setPhoneNumber(phoneNumber);
+
+        if (studentBook != null)
+            user.setStudentsBookNumber(studentBook);
+
+        if (groupId != null) {
+            try {
+                user.setGroup(groupService.findById(groupId));
+            } catch (GroupNotFoundException e) {
+                e.printStackTrace();
+                return "redirect:/admin";
+            }
+        }
 
         try {
             userService.addUser(user);
         } catch (UserAlreadyExistsException e) {
             e.printStackTrace();
         }
+
         return "redirect:/admin";
     }
+
+    @PostMapping("/admin/group/add")
+    public String groupAdd(@RequestParam("groupname") String name, @RequestParam("groupcourse") Byte course,
+                           @RequestParam("groupnumber") Byte number) {
+        Group newGroup = new Group();
+        newGroup.setName(name);
+        newGroup.setCourse(course);
+        newGroup.setNumber(number);
+
+        try {
+            groupService.addGroup(newGroup);
+        } catch (GroupAlreadyExistsException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/admin";
+    }
+
 }
