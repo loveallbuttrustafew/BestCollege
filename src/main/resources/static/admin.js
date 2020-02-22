6c437b32d93bd963b49875a4d34648b34d52ce4c
Nick <nick>
2020-02-22 15:39:39 +0300

    Реконстурирование и много правок

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -18,22 +18,73 @@ menu.addEventListener('mouseout', function () {
 // Всплывающая форма
 let add = document.getElementById('add'); // Кнопка для всплывающей формы добавки юзера и группы
 let add_form = document.getElementById('add_toast_form'); // всплывающая форма добавки юзера и группы
-let add_group_form = document.getElementById('add_group_wrapper'); // форма для добавки группы
-let type_radio = document.getElementsByName('type_user'); // массив радиобаттонов для выбора типа юзера
-const close_btn = document.getElementById('close_toast_form_button'); // кнопка закрытия формы
-let form_for_admin_or_user = document.getElementById('users_toast_form');//форма добавки админа или препода
-close_btn.addEventListener('click', function () {
-    add_form.style.display = 'none';
-});
 add_admin_or_teacher.addEventListener('click', function () {
     add_form.style.display = 'block';
+    add_form.innerHTML = `
+    <!-------Кнопка закрытия формы------------>
+    <div id="close_toast_form_button">
+        <!--Кнопка закрыть-->
+    </div>
+    <form action="/admin/user/add" method="POST" id="users_toast_form">
+    <input name="_csrf" type="hidden" value="{{_csrf.token}}" />
+    <div id="add_user_wrapper">
+        <div id="users">
+            <div> Преподаватель<input type="radio" name="typeuser" value="TEACHER">Администратор<input
+                    type="radio" name="typeuser" value="ADMIN"></div>
+            <div><input type="text" name="firstname" placeholder="Имя"></div>
+            <div><input type="text" name="secondname" placeholder="Фамилия"></div>
+            <div><input type="text" name="lastname" placeholder="Отчество"></div>
+            <div><input type="text" name="phonenumber" placeholder="Номер телефона"></div>
+            <div><input type="text" name="username" placeholder="Логин"></div>
+            <div><input type="password" name="password" placeholder="Пароль"></div>
+            <div id="sub_users">
+            <input type="submit" value="Добавить">
+        </div>
+    </div>
+</form>
+    `;
+    let close_btn = document.getElementById('close_toast_form_button'); // кнопка закрытия формы
+    close_btn.addEventListener('click', function () {
+        add_form.style.display = 'none';
+    });
+    let form_for_admin_or_user = document.getElementById('users_toast_form');//форма добавки админа или препода
     form_for_admin_or_user.style.display = 'block';
-    add_group_form.style.display = 'none';
 });
 add.addEventListener('click', function () {
     add_form.style.display = 'block';
+    add_form.innerHTML = `
+    <!-------Кнопка закрытия формы------------>
+    <div id="close_toast_form_button">
+        <!--Кнопка закрыть-->
+    </div>
+    <form action="/admin/group/add" method="POST">
+            <input name="_csrf" type="hidden" value="{{_csrf.token}}" />
+            <div id="add_group_wrapper">
+                <div id="add_group_container">
+                    <div id="group_name">
+                        <input type="text" name="groupname" placeholder="Наименование группы:">
+                    </div>
+
+                    <div>
+                        <input type="text" name="groupcourse" id="course" placeholder="Курс:">
+                    </div>
+
+                    <div id="group_number">
+                        <input type="text" name="groupnumber" placeholder="Номер группы:">
+                    </div>
+                    <div id="sub_group">
+                            <input type="submit" value="Добавить">
+                    </div>
+                </div>
+            </div>
+        </form>
+    `;
+    let close_btn = document.getElementById('close_toast_form_button'); // кнопка закрытия формы
+    close_btn.addEventListener('click', function () {
+        add_form.style.display = 'none';
+    });
+    let add_group_form = document.getElementById('add_group_wrapper'); // форма для добавки группы
     add_group_form.style.display = 'block';
-    form_for_admin_or_user.style.display = 'none';
 });
 
 
@@ -84,28 +135,34 @@ xhr.addEventListener('readystatechange', function () {
                     let user_toast_form = document.getElementById('user_toast_form');
                     let discipline_toast_form = document.getElementById('discipline_toast_form');
                     add_user_form.addEventListener('click', function () {
-                    discipline_toast_form.style.display = 'none';
-                     user_toast_form.style.display = 'block';
+                        discipline_toast_form.style.display = 'none';
+                        user_toast_form.style.display = 'block';
                     });
-                  add_discipline_form.addEventListener('click', function () {
-                    discipline_toast_form.style.display = 'block';
-                   user_toast_form.style.display = 'none';
-                  });
+                    add_discipline_form.addEventListener('click', function () {
+                        discipline_toast_form.style.display = 'block';
+                        user_toast_form.style.display = 'none';
+                    });
 
                     for (let i = 0; i < discipline_list.length; i++) {
                         discipline_list[i].addEventListener('click', function () {
-                            form_for_admin_or_user.style.display = 'none';
-                            add_group_form.style.display = 'none';
                             let xhr2 = new XMLHttpRequest();
                             dispId = document.querySelectorAll('#group_info #about_group #discipline_wrapper table tbody tr');
-                           
+
                             dispId.forEach(element => {
                                 element.addEventListener('click', function () {
                                     xhr2.open('GET', '/admin/subject/info?id=' + element.getAttribute('subject_id'));
                                     xhr2.send();
                                     xhr2.addEventListener('readystatechange', function () {
+
                                         add_form.innerHTML = this.responseText;
+                                        all_group_info.style.display = 'none';
+                                        all_group_info.style.display = 'block';
                                         add_form.style.display = 'block';
+
+                                        close_btn2 = document.getElementById('close_toast_button');
+                                        close_btn2.addEventListener('click', function () {
+                                            add_form.style.display = 'none';
+                                        });
                                     });
                                 });
                             });
