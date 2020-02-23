886595f77ba3a642af3e6bfee09169c0132ab16e
Nick <nick>
2020-02-23 18:38:03 +0300

    сделал

--- src/main/resources/static/teacher.js
+++ src/main/resources/static/teacher.js
@@ -14,3 +14,26 @@ menu.addEventListener('mouseout', function () {
     exit.style.backgroundImage = '';
 });
 
+// обработчик нажатия на предмент
+
+let discipline_list = document.querySelectorAll('#discipline_list li');
+let discp_info = document.getElementById('info');
+let sub_value = document.getElementById('sub_value');
+discipline_list.forEach(element => {
+    element.addEventListener('click',get_info);
+});
+function get_info(){
+    for(let i = 0;i<discipline_list.length;i++){
+        if(discipline_list[i].classList.contains('select')){
+            discipline_list[i].classList.remove('select');
+            discipline_list[i].style.backgroundColor = 'rgba(255,255,255,0.8)';
+            discipline_list[i].style.color = '#000000';
+        }
+    }
+    this.classList.add('select');
+    this.style.color = '#ffffff';
+    this.style.backgroundColor = '#7908AA';
+    let info = this.textContent;
+    discp_info.innerHTML = "<div><h2>" + info + "</h2></div>"; 
+    sub_value.value = this.getAttribute('subjectid');
+};
